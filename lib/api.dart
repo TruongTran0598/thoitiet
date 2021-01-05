7f85b307aeef918f3d251ed60ea079e136fbca7d
TruongTran0598 <truongtran0598@gmai.com>
2021-01-06 00:13:10 +0700

    style[api and homepage] change something

--- lib/api.dart
+++ lib/api.dart
@@ -7,14 +7,17 @@ class Api{
   Future<DataJson> getInformationWeatherInHanoi({Function(String) onError}) async {
     String endpoint = 'http://api.openweathermap.org/data/2.5/weather?q=HaNoi&units=metric&appid=edd18907326b3d7c678a223bd45bfb1b';
     http.Response response = await http.get(endpoint);
+    dynamic data;
     if(response.statusCode == 200){
       try{
-        dynamic data = json.decode(response.body);
-        print(data);
+        data = json.decode(response.body);
+        print("data: ${DataJson.formJson(data)}");
         return DataJson.formJson(data);
       }catch(error){
-
+        onError("Something wrong");
       }
+    }else{
+      onError("Something error. Status code ${response.statusCode}");
     }
   }
 }
